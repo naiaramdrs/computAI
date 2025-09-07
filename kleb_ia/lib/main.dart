7d7e4e397b0917a9ae3e5a50758c2b441d8d25e8
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2025-09-07 18:38:06 -0300

    chore: finish google tutorial

--- kleb_ia/lib/main.dart
+++ kleb_ia/lib/main.dart
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
       case 0:
         page = GeneratorPage();
       case 1:
-        page = Placeholder();
+        page = FavoritePage();
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
@@ -167,5 +167,39 @@ class MyAppState extends ChangeNotifier {
       favorites.add(current);
     }
     notifyListeners();
+  }
+}
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.primary,
+    );
+    
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.', style: style),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:',
+              style: style),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase, style: style),
+          ),
+      ],
+    );
   }
 }
--- kleb_ia/test/widget_test.dart
+++ kleb_ia/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:kleb_ia/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyAppState() as Widget);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
