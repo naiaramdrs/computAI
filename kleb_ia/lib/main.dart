707126ece69eb4331e8887da95de33dd88df868e
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2025-09-07 23:07:09 -0300

    chore: mainPage

--- kleb_ia/lib/main.dart
+++ kleb_ia/lib/main.dart
@@ -9,13 +9,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'KlebIA',
-      home: const SimplePage(),
+      home: const MainPage(),
     );
   }
 }
 
-class SimplePage extends StatelessWidget {
-  const SimplePage({super.key}); 
+class MainPage extends StatelessWidget {
+  const MainPage({super.key}); 
 
   static final ThemeData theme = ThemeData(
     primarySwatch: Colors.indigo,
@@ -28,14 +28,68 @@ class SimplePage extends StatelessWidget {
     );
 
     return Scaffold(
+      backgroundColor: theme.colorScheme.secondary,
       body: Center(
         child: Text(
-            'Sua assistente virtual de\nComputação@UFCG',
+          'Sua assistente virtual de\nComputação@UFCG',
           style: style,
           textAlign: TextAlign.center,
         ),
       ),
-      backgroundColor: theme.colorScheme.secondary,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: InputField(),
+      ),
+    );
+  }
+}
+
+class InputField extends StatefulWidget {
+  const InputField({
+    super.key,
+  });
+
+  @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(),
+        hintText: 'Digite sua dúvida aqui',
+        suffixIcon: SendButton(
+          onSend: () {
+            print(_controller.text);
+          },
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide(color: Colors.grey.shade300),
+        ),
+      ),
+    );
+  }
+}
+
+class SendButton extends StatelessWidget {
+  final VoidCallback onSend;
+
+  const SendButton({super.key, required this.onSend});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_circle_up),
+      color: Colors.indigo,
+      onPressed: onSend,
     );
   }
 }
