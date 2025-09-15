c45920806d258148756526917dadaf994c99de94
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-09-15 18:24:44 -0300

    chore: altera nome de variável

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from core.chat import create_chat
 
 load_dotenv()
 
-data_sites = pd.read_csv('./data/csv/data_site.csv')
+data_sites = pd.read_csv('./data/csv/data_sites.csv')
 data_pdf = pd.read_csv('./data/csv/data_pdf.csv')
 
 embeddings = get_embeddings_model()
@@ -15,8 +15,8 @@ vector_store = get_vector_store(embeddings)
 retriever = get_retriever(vector_store)
 
 ## Essa parte vamos rodar apenas uma vez, no momento de adicionar os dados no vector database
-text = string_transform(data_sites) + string_transform(data_pdf)
-documents, ids = create_chunks(text)
-add_documents(documents, ids, vector_store)
+# text = string_transform(data_sites) + string_transform(data_pdf)
+# documents, ids = create_chunks(text)
+# add_documents(documents, ids, vector_store)
 
 print(create_chat("Como é o curso de computacao?", retriever))
