520b4158039c83ee05e6129b94208fe826ba5240
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-09-15 19:15:01 -0300

    feat: cria API para requisições

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+import uuid
+from flask import Flask, jsonify, request
+from core.models import get_embeddings_model
+from core.vector_store import get_vector_store, get_retriever
+from core.chat import create_chat
+
+app = Flask(__name__)
+
+embeddings = get_embeddings_model()
+vector_store = get_vector_store(embeddings)
+retriever = get_retriever(vector_store)
+
+@app.route("/chat", methods=["POST"])
+def create_chat():
+    chat_id = uuid.uuid4()
+    data = request.get_json()
+    answer = create_chat(data.get('question'), retriever, chat_id)
+    return jsonify({"message": "Prompt enviado com sucesso!", "answer": answer, "chat_id": chat_id}), 201
+
+@app.route("/chat/<chat_id>/question", methods=["POST"])
+def send_question(chat_id):
+    data = request.get_json()
+    answer = create_chat(data.get('question'), retriever, chat_id)
+    return jsonify({"message": "Prompt enviado com sucesso!", "answer": answer}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
