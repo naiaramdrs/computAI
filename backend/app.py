ffad1205bfbf5ea14956a39cb07974ba35d5f9d8
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-09-16 16:44:54 -0300

    chore: adiciona cors

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 import uuid
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from core.models import get_embeddings_model
 from core.vector_store import get_vector_store, get_retriever
 from core.chat import get_answer
 
 app = Flask(__name__)
+CORS(app)
 
 embeddings = get_embeddings_model()
 vector_store = get_vector_store(embeddings)
@@ -12,7 +14,7 @@ retriever = get_retriever(vector_store)
 
 @app.route("/chat", methods=["POST"])
 def create_chat():
-    chat_id = uuid.uuid4()
+    chat_id = str(uuid.uuid4())
     data = request.get_json()
     answer = get_answer(data.get('question'), retriever, chat_id)
     return jsonify({"message": "Prompt enviado com sucesso!", "answer": answer, "chat_id": chat_id}), 201
