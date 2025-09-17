9f50622e6f096d6412dc8225a8d166abfb8b369a
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2025-09-16 22:36:43 -0300

    chore: rename folder

---
+++ frontend/lib/pages/main_page.dart
@@ -0,0 +1,75 @@
+import 'package:klebia/widgets/input_field.dart';
+import 'package:klebia/pages/chat_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo.shade300,
+      body: Column(
+        children: [
+          Expanded(child: WelcomePage()),
+          InputField(
+            onSend: (text) {
+              if (text.isNotEmpty) {
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        ChatPage(submittedText: text),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          const curve = Curves.easeInOut;
+                          final tween = Tween(
+                            begin: begin,
+                            end: end,
+                          ).chain(CurveTween(curve: curve));
+                          return SlideTransition(
+                            position: animation.drive(tween),
+                            child: child,
+                          );
+                        },
+                    transitionDuration: const Duration(milliseconds: 700),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(
+      context,
+    ).textTheme.titleSmall!.copyWith(fontWeight: FontWeight.bold);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/logo_vertical.png', width: 200, height: 200),
+          Text(
+            'Sua assistente virtual de\nComputação@UFCG',
+            style: style,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- kleb_ia/android/app/src/main/kotlin/com/example/kleb_ia/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kleb_ia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
